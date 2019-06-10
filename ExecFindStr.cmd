7fe4969cb1cfb9c1bf100874651203edc5a69e0e
Pic0o <47042955+botsama@users.noreply.github.com>
2019-06-10 10:48:53 -0400

    Update ExecFindStr.cmd

--- ExecFindStr.cmd
+++ ExecFindStr.cmd
@@ -1,3 +1,3 @@
 @echo off
 @echo Searching local disk for patterns defined in .ps1 script.
-PowerShell.exe -ExecutionPolicy Bypass -File findStrSga_extensionsConfig.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File findStrSite_extensionsConfig.ps1
