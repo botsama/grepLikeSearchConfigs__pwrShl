c46d046e997df8dcf8c8ab3275faadd1b58c5f06
Pic0o <47042955+botsama@users.noreply.github.com>
2019-10-31 23:17:59 -0400

    remove prior comments
    see extra debug details in below cards:
    https://github.com/users/botsama/projects/1#card-28590496
    https://github.com/users/botsama/projects/1#card-28590626

--- findFile.ps1
+++ findFile.ps1
@@ -1,4 +1,4 @@
-﻿# Search for files matching a pattern.
+# Search for files matching a pattern.
 # testing to see if quicker than running a findstr on cmd line.
 
 $boxName = $env:COMPUTERNAME
@@ -24,17 +24,3 @@ $bGetQual03 | Select Path | Get-Unique -AsString | Export-Csv -Path .\LocalSearc
 
 $bGetQual04 = $buildSearchStr | Select-String "$wherePattern04"
 $bGetQual04 | Select Path | Get-Unique -AsString | Export-Csv -Path .\LocalSearches_"$wherePattern04"_RemoveDupes_PathToFiles.csv
-
-# Example: Outputs to a CSV.
-#$bGetQual03 = $buildSearchStr | Select-String "$wherePattern03"
-#$bGetQual03 | Select Path | Get-Unique -AsString | Export-Csv -Path .\LocalSearches_"$wherePattern03"_RemoveDupes_PathToFiles.csv
-
-# Get-ChildItem has picky syntax so I opted to define the full line with -Includes instead of inserting the extensions.
-# -Error Action SilentlyContinue lets the script proceed without dropping errors for protected OS folders that are not relevant to searches.
-# 2nd part of Pipe command processes all files matching the extensions defined in the Get-ChildItem includes.
-# 3rd Pipe says to only output the full file path. Details for filtering based off the MatchInfo Class
-# Link https://docs.microsoft.com/en-us/dotnet/api/microsoft.powershell.commands.matchinfo?view=pscore-6.0.0
-# Default params output the line said string is found on, but the output is quite sloppy to parse.
-# 4th Pipe purpose is to output the full file path without cutting off characters.
-# 5th and final pipe of command outputs files found matching said string into the local folder as .log files, this script is running from.
- 
